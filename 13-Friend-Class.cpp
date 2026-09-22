acd4797247f92d7008abd6755a7c91a64ffe091d
baravebhumika17-spec <baravebhumika17@gmail.com>
2026-09-22 16:31:19 +0530

    Add Account and Auditor classes with friend access

---
+++ 13-Friend-Class.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Account {
+private:
+    double balance;
+
+    friend class Auditor;
+
+public:
+    Account(double initialBalance) {
+        balance = initialBalance;
+    }
+};
+
+class Auditor {
+public:
+    void inspect(const Account& account) const {
+        cout << "Account Balance: "
+             << account.balance << endl;
+    }
+};
+
+int main() {
+    Account account(5000.0);
+
+    Auditor auditor;
+    auditor.inspect(account);
+
+    return 0;
+}
